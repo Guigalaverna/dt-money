43ef94b2eda43f7e39e3ea09c1079dfc2b1f4d5a
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 10:30:35 -0300

    Using Poppins font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,6 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #F0F2F5;
     --shape: #FFFFFF;
   }
+
   * {
     margin: 0;
     padding: 0;
@@ -33,6 +34,14 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Poppins', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
