aae07e2c21dd0b96d6a050d01510ffb9ec76ee6f
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 11:03:00 -0300

    Import logo from assets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import { Container, Wrapper } from "./styles";
 
-import LogoImage from "../../assets/logo.svg";
+import { Logo } from "../../assets/";
 
 export function Header() {
   return (
     <Container>
       <Wrapper>
-        <img src={LogoImage} alt="dt money logo" />
+        <img src={Logo} alt="dt money logo" />
         <button type="button">Nova transação</button>
       </Wrapper>
     </Container>
