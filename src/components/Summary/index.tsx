bd1b2026e78eb85d2d23bfebce87dbc9b6aee567
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 11:02:45 -0300

    Create summary cards

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,30 @@
+import { Income, Outcome, Total } from "../../assets";
 import { Container } from "./styles";
 
 function Summary() {
   return (
     <Container>
-      <h1>Summary</h1>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={Income} alt="Entradas" />
+        </header>
+        <strong>R$ 1.000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={Outcome} alt="Saídas" />
+        </header>
+        <strong>- R$ 500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={Total} alt="Total" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
     </Container>
   );
 }
