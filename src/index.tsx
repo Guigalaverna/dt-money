67de972139c8d5e0388aa43aff7c567387fba884
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 12:51:21 -0300

    Create post route and setup database

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Server } from "miragejs";
+import { Model, Server } from "miragejs";
 import { App } from "./App";
 import { GlobalStyle } from "./styles/global";
 
 new Server({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Desenvolvimento de Website",
-          amount: 400,
-          type: "income",
-          category: "Trabalho",
-        },
-        {
-          id: 2,
-          title: "Desenvolvimento de Website",
-          amount: 500,
-          type: "income",
-          category: "Trabalho",
-        },
-        {
-          id: 3,
-          title: "Desenvolvimento de Website",
-          amount: 600,
-          type: "income",
-          category: "Trabalho",
-        },
-        {
-          id: 4,
-          title: "Desenvolvimento de Website",
-          amount: 700,
-          type: "income",
-          category: "Trabalho",
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
