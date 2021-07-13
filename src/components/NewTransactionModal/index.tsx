dc9eded79f9caebddd0119838802293e3aacf56b
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 12:21:41 -0300

    Create modal buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import Modal from "react-modal";
 
-import { Container } from "./styles";
-
+import { Container, ButtonsWrapper } from "./styles";
+import { Close, Income, Outcome } from "../../assets";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -20,10 +20,25 @@ export function NewTransactionModal({
       className="react-modal-content"
       overlayClassName="react-modal-overlay"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={Close} alt="Fechar modal" />
+      </button>
       <Container>
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
+        <ButtonsWrapper>
+          <button type="button">
+            <img src={Income} alt="Entrada" /> <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={Outcome} alt="Saída" /> <span>Saída</span>
+          </button>
+        </ButtonsWrapper>
         <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
