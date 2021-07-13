831fe61be899ac776aaac34fa4d4bb2a2f5bd79c
Guigalaverna <guigalaverna@gmail.com>
2021-07-13 11:03:28 -0300

    Export all assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Close from "./close.svg";
+import Income from "./income.svg";
+import Logo from "./logo.svg";
+import Outcome from "./outcome.svg";
+import Total from "./total.svg";
+
+export { Close, Income, Logo, Outcome, Total };
